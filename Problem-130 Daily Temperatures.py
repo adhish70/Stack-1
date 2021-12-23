36b9e0efd5c97b664c02639a147376cd76b3e988
Adhish Shrivastava <adhish70@gmail.com>
2021-12-23 11:37:39 -0500

    Initial Commit

---
+++ Problem-130 Daily Temperatures.py
@@ -0,0 +1,11 @@
+# 739. Daily Temperatures
+# https://leetcode.com/problems/daily-temperatures/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        pass
---
+++ Problem-131 Next Greater Element II.py
@@ -0,0 +1,11 @@
+# 503. Next Greater Element II
+# https://leetcode.com/problems/next-greater-element-ii/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        pass
